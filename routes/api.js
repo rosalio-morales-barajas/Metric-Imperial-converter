f93141e8c18a801131485835eb766a75ce88cd16
Saif <saiofa99@gmail.com>
2022-01-27 20:49:12 +0300

    init commit

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const expect = require('chai').expect;
+const ConvertHandler = require('../controllers/convertHandler.js');
+
+module.exports = function (app) {
+  
+  let convertHandler = new ConvertHandler();
+
+  app.route('/api/convert').get((req, res) => {
+    console.log('convert')
+  })
+
+};
